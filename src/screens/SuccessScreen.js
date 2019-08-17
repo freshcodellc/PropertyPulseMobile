198cbf44bc93bb92ad781122604ce4aa4accd3ba
Richard Macdonald <macdrich@gmail.com>
2019-08-17 10:29:14 -0600

    completes requested changes on the success screen

--- src/screens/SuccessScreen.js
+++ src/screens/SuccessScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
+const MEME_THANK_YOU = require('../images/thank-you-meme.jpg')
+
 const Container = styled.View`
   flex: 1;
   background-color: #fff;
@@ -10,18 +12,39 @@ const Container = styled.View`
   padding-right: 15%;
 `
 
-const ThankYouText = styled.Text`
+const MemeContainer = styled.View`
+  margin-bottom: 36;
+  margin-top: 24;
+`
+
+const MemeHolder = styled.Image`
+  height: 270px;
+  width: 480px;
+`
+
+const PrimaryText = styled.Text`
   font-size: 48;
   text-align: center;
-  margin-bottom: 30;
+  margin-bottom: 24;
   color: #5C5C5C;
 `
 
-export default function SuccessScreen(props) {
+const SecondaryText = styled.Text`
+  font-size: 24;
+  text-align: center;
+  margin-bottom: 12;
+  color: #6E6E6E;
+`
+
+export default function SuccessScreen (props) {
   setTimeout(() => props.navigation.popToTop(), 5000)
   return (
     <Container>
-      <ThankYouText>Thank you for your input!</ThankYouText>
+      <MemeContainer>
+        <MemeHolder source={MEME_THANK_YOU } />
+      </MemeContainer>
+      <PrimaryText>Keep an eye out for our kiosks.</PrimaryText>
+      <SecondaryText>(Most people vote weekly)</SecondaryText>
     </Container>
   )
 }
